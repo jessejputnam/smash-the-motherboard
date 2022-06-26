4ce30fd7f536231d42481c77dd1ed217425192de
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-06-26 08:54:05 -0400

    Clean up CRA; add UserPage and LandingPage

---
+++ src/components/NavPanel.js
@@ -0,0 +1,54 @@
+import "../styles/NavPanel.css";
+
+const ErrNavPanel = () => {
+  return (
+    <div className='ErrNavPanel'>
+      <p>Error: improper user-type for NavPanel</p>
+    </div>
+  );
+};
+
+const CreatorNavPanel = () => {
+  return <div className='CreatorNavPanel'></div>;
+};
+
+const PatronNavPanel = () => {
+  return (
+    <div className='PatronNavPanel'>
+      <div className='panel__nav'>
+        <button type='button' className='btn--nav-panel'>
+          Account
+        </button>
+      </div>
+      <div className='panel__nav'>
+        <button type='button' className='btn--nav-panel'>
+          Search
+        </button>
+      </div>
+      <div className='panel__nav'>
+        <button type='button' className='btn--nav-panel'>
+          Subs
+        </button>
+      </div>
+      <div className='panel__nav'>
+        <button type='button' className='btn--nav-panel'>
+          Settings
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const NavPanel = (props) => {
+  const userType = props.userType;
+
+  let panel;
+
+  if (userType === "patron") panel = <PatronNavPanel />;
+  else if (userType === "creator") panel = <CreatorNavPanel />;
+  else panel = <ErrNavPanel />;
+
+  return <div className='NavPanel'>{panel}</div>;
+};
+
+export default NavPanel;
---
+++ src/pages/UserPage.js
@@ -0,0 +1,11 @@
+import NavPanel from "../components/NavPanel";
+
+const UserPage = () => {
+  return (
+    <div className='UserPage'>
+      <NavPanel userType='patron' />
+    </div>
+  );
+};
+
+export default UserPage;
