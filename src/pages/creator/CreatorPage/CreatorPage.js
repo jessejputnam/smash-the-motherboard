8bb2c6794f893c4add1f566bd3aad1e500a6ebc1
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-07-13 11:54:03 -0400

    Add profile image, project title, project descriptions to Creator page

--- src/pages/creator/CreatorPage/CreatorPage.js
+++ src/pages/creator/CreatorPage/CreatorPage.js
@@ -6,6 +6,20 @@ import styles from "./CreatorPage.module.css";
 
 // Import images
 import bannerPlaceholder from "../../../assets/images/banner-placeholder.png";
+import profilePlaceholder from "../../../assets/images/userPlaceholder.png";
+import editIcon from "../../../assets/icons/edit-icon.svg";
+
+const EditBtn = () => {
+  const getEl = (e) => {
+    console.log(e.target.parentElement);
+  };
+
+  return (
+    <button onClick={getEl} className={styles.EditBtn}>
+      <img src={editIcon} alt='Edit' />
+    </button>
+  );
+};
 
 const CreatorPage = (props) => {
   // const [name, setName] = useState("");
@@ -14,8 +28,6 @@ const CreatorPage = (props) => {
   // const [medium, setMedium] = useState("");
   // const [keywords, setKeywords] = useState("");
   const [banner, setBanner] = useState(bannerPlaceholder);
-
-  //TODO Become creator adds creator to user db, change in dashboard state, rerender this page with new props
 
   const becomeCreator = () => {
     props.becomeCreator({ field: "creator", value: true });
@@ -27,19 +39,39 @@ const CreatorPage = (props) => {
 
   return (
     <div className={styles.CreatorPage}>
-      <h1>Creator Acount</h1>
+      <h1>Creator Page</h1>
       {props.isCreator === false ? (
         <button onClick={becomeCreator} className={styles.createBtn}>
           Become a Creator
         </button>
       ) : null}
-      <main>
-        <div
+      <div className={styles.pageContainer}>
+        <header
           className={styles.banner}
           style={{ backgroundImage: `url(${banner})` }}
-        ></div>
+        >
+          <EditBtn />
+          <div className={styles.profileContainer}>
+            <img src={profilePlaceholder} alt='Profile' />
+            <EditBtn />
+          </div>
+        </header>
+
+        <div className={styles.mainContainer}>
+          <h1>Project Title</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <footer></footer>
         <button onClick={test}>Test</button>
-      </main>
+      </div>
     </div>
   );
 };
